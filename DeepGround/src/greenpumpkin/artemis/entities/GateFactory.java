7d2a3a64250f0fa415e65745b2e156027a1fa6f3
thisisdaleb <officialgsproductions@gmail.com>
2014-08-16 13:41:37 -0500

    Added sprite to Yellow Gate

--- DeepGround/src/greenpumpkin/artemis/entities/GateFactory.java
+++ DeepGround/src/greenpumpkin/artemis/entities/GateFactory.java
@@ -1,13 +1,27 @@
 package greenpumpkin.artemis.entities;
 
+import greenpumpkin.artemis.components.SpriteC;
+
 import com.artemis.Entity;
 import com.artemis.World;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class GateFactory {
 	
 	public static Entity createYellowHorizontalGate(World world, float x, float y) {
 		Entity e = world.createEntity();
+		
 		System.out.println("Horizontal yellow gate created at: " + x + ", " + y);
+		Texture texture = new Texture(Gdx.files.internal("DGItems/YellowGate.png"));
+		
+		SpriteC sprite = new SpriteC();
+		sprite.sprite = new Sprite(texture);
+		sprite.sprite.setScale(1/16f);
+		sprite.sprite.setOrigin(0, 0);
+		e.addComponent(sprite);
+		
 		return e;
 	}
 	
